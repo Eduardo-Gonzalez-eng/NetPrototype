57934b17d661cb9f87e38534f18dd1357c26146d
Eduardo-Gonzalez-eng <eduardo.trabajos12@gmail.com>
2025-02-27 17:59:12 -0300

    Se ha arreglado el diseño del Selector

--- NetPrototype/obj/Debug/net8.0-windows/NetPrototype.AssemblyInfo.cs
+++ NetPrototype/obj/Debug/net8.0-windows/NetPrototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetPrototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a84e5a0aa218829eb3f304981229669f0bdd27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fae2339ad208bab1dac18eb10ca445f8963c293")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetPrototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetPrototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
